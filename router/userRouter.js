13bc8b9386ee3ad4f2df35a4821a6a9b8f7dc244
Abhishek-43 <144531926+Abhishek-43@users.noreply.github.com>
2024-06-16 12:18:08 +0530

    Update userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { addNewAdmin,addNewDoctor,getAllDoctors,getUserDetails,login,logoutAdmin,logoutPatient,patientRegister } from "../controller/usercontroller.js";
+import { addNewAdmin,addNewDoctor,getAllDoctors,getUserDetails,login,logoutAdmin,logoutPatient,patientRegister } from "../controller/userController.js";
 import { isAdminAuthenticated, isPatientAuthenticated} from "../middlewares/auth.js";
 
 const router = express.Router();
